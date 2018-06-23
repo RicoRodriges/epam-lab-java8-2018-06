eecdbc2d65292db3a3b95fbe0c9dff2a916db1be
RicoRodriges <Dimon_dem@bk.ru>
2018-06-23 13:18:44 +0300

    Completed stream part 2 exercise 4

--- src/test/java/streams/part2/exercise/Exercise4.java
+++ src/test/java/streams/part2/exercise/Exercise4.java
@@ -3,8 +3,12 @@ package streams.part2.exercise;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
+import static java.util.stream.Collectors.*;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise4 {
@@ -13,12 +17,22 @@ public class Exercise4 {
      * Выбирает из текста наиболее часто встречающиеся слова.
      * Подсчет слов выполняется без учета их регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово.
      * Если некоторые слова имеют одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
-     * @param text Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
+     *
+     * @param text        Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
      * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо отобрать.
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+        Map<String, Long> frequencyOfWords = Pattern.compile("\\s+").splitAsStream(text)
+                .map(String::toLowerCase)
+                .collect(groupingBy(string -> string, counting()));
+        return frequencyOfWords.entrySet().stream()
+                .sorted(Comparator.<Map.Entry<String, Long>>
+                        comparingLong(Map.Entry::getValue).reversed()
+                        .thenComparing(Map.Entry::getKey))
+                .limit(numberWords)
+                .map(Map.Entry::getKey)
+                .collect(toList());
     }
 
     @Test
@@ -33,10 +47,10 @@ public class Exercise4 {
     @Test
     public void test2() {
         String source = "Lorem ipsum dolor sit amet consectetur adipiscing elit Sed sodales consectetur purus at "
-                      + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
-                      + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
-                      + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
-                      + "Integer vel odio nec mi tempor dignissim";
+                + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
+                + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
+                + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
+                + "Integer vel odio nec mi tempor dignissim";
 
         List<String> result = getFrequentlyOccurringWords(source, 10);
 
