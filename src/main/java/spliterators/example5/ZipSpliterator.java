32dfa685ac8f409d794366c4798d3f2098ac4b3d
RicoRodriges <dimon_dem@bk.ru>
2018-06-29 16:05:47 +0300

    Completed spliterators exercise 3

--- src/main/java/spliterators/example5/ZipSpliterator.java
+++ src/main/java/spliterators/example5/ZipSpliterator.java
@@ -1,20 +1,29 @@
 package spliterators.example5;
 
+import java.security.InvalidParameterException;
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
+import spliterators.example4.Pair;
 
-public class ZipSpliterator<T> extends Spliterators.AbstractSpliterator<T> {
+public class ZipSpliterator<T, U> extends Spliterators.AbstractSpliterator<Pair<T, U>> {
 
-    public ZipSpliterator() {
-        // TODO implementation
-        super(0, 0);
+    private Spliterator<T> spliteratorLeft;
+    private Spliterator<U> spliteratorRight;
+
+    public ZipSpliterator(Spliterator<T> spliteratorLeft, Spliterator<U> spliteratorRight) {
+        super(spliteratorLeft.estimateSize(), spliteratorLeft.characteristics() & ~(CONCURRENT));
+        if (spliteratorLeft.getExactSizeIfKnown() != spliteratorRight.getExactSizeIfKnown()) {
+            throw new InvalidParameterException();
+        }
+        this.spliteratorLeft = spliteratorLeft;
+        this.spliteratorRight = spliteratorRight;
     }
 
     @Override
-    public boolean tryAdvance(Consumer<? super T> action) {
-        // TODO implementation
-        throw new UnsupportedOperationException();
+    public boolean tryAdvance(Consumer<? super Pair<T, U>> action) {
+        return spliteratorLeft.tryAdvance(
+            (x) -> spliteratorRight.tryAdvance(
+                (y) -> action.accept(new Pair<>(x, y))));
     }
-
-    // TODO other methods of spliterator
 }
